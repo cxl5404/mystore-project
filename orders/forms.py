662b0b2287c39ca6642c2afba6449de07444686a
cxl5404 <cxl5404@gmail.com>
2018-04-05 23:32:22 -0400

    Chinese Version

--- orders/forms.py
+++ orders/forms.py
@@ -5,3 +5,12 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        labels = {
+            'first_name': ('名字'),
+            'last_name': ('姓'),
+
+            'address': ('地址'),
+            'postal_code': ('邮编'),
+            'city': ('城市'),
+
+        }
