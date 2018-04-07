b89c926a8b1ca2bb68e71811335e1d45bc061bd8
cxl5404 <cxl5404@gmail.com>
2018-04-07 16:38:26 -0400

    email

--- payment/views.py
+++ payment/views.py
@@ -32,7 +32,7 @@ def payment_process(request):
         from_email = 'cxl5404@gmail.com.com'
         to = 'cxl5404@gmail.com'
         text_content = 'This is an important message.'
-        html_content = '<p>An <strong>important</strong> message.</p>'
+        html_content = render_to_string('email.html', {'order_id': order_id})
         msg = EmailMultiAlternatives(subject,
                              text_content, from_email, [to])
         msg.attach_alternative(html_content, "text/html")
