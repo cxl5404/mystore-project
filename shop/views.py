0ecd1b6661eb0b86912d8c93dcce59fdb5605efb
cxl5404 <cxl5404@gmail.com>
2018-04-09 20:42:50 -0400

    slider

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
 
 
 def product_list(request, category_slug=None):
+    limit = 8  # 每页显示的记录数
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    paginator = Paginator(products, limit)  # 实例化一个分页对象
+    page = request.GET.get('page')  # 获取页码
+    try:
+        products = paginator.page(page)  # 获取某页对应的记录
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
     if category_slug:
+        limit = 8
+        products = Product.objects.filter(available=True)
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+        paginator = Paginator(products, limit)  # 实例化一个分页对象
+        page = request.GET.get('page')  # 获取页码
+        try:
+            products = paginator.page(page)  # 获取某页对应的记录
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
+
     return render(request, 'shop/product/list.html', {'category': category,
                                                       'categories': categories,
                                                       'products': products})
